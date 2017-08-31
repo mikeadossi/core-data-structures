1e78bc9bdd9e777a44e6eaffe139ae8205448b5c
mikeadossi <mikeadossi@gmail.com>
2017-08-30 17:06:28 -0700

    directions written to run your stack ds

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,4 @@
-/* RUN TESTS ON CODE BELOW BY RUNNING 'npm test' in src directory */
-export default class Stack {
+class Stack {
 
   constructor() {
     this.dataStore = []
@@ -7,17 +6,21 @@ export default class Stack {
   }
   push(element) {
     this.dataStore[this.top++] = element;
+    console.log('pushed!');
   }
   pop() {
     return this.dataStore[--this.top];
   }
   peek() {
+    console.log(this.dataStore[this.top-1])
     return this.dataStore[this.top-1]
   }
   isEmpty() {
+    console.log(this.dataStore.length <= 0)
     return this.dataStore.length <= 0
   }
   length() {
+    console.log(this.top);
     return this.top
   }
   returnStackedElement(indexPos){
@@ -25,7 +28,15 @@ export default class Stack {
       console.log('popped element on index '+this.top)
       this.top = i
     }
+    console.log(this.dataStore[this.top])
     return this.dataStore[this.top]
   }
-  
 }
+
+// below we share an example of how you might console log the results of our stack ds in your CLI.
+var stack = new Stack()
+stack.push('10');
+stack.length();
+stack.returnStackedElement();
+
+module.exports = Stack;
